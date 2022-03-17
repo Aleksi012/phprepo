ddbf1096e23b4345d5b37fdded76d88ae68eb293
Aleksi012 <aleksi.anttila001@gmail.com>
2022-03-17 19:44:34 +0200

    ilman amount

--- index.php
+++ index.php
@@ -1,26 +1,14 @@
 <?php
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Credentials: true');
-header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
-header('Access-Control-Allow-Headers: Content-Type');
-header('Access-Control-Max-Age: 3600');
-header('Content-Type: application/json');
-
-
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    return 0;
-}
+require_once 'inc/header.php';
+require_once 'inc/functions.php';
 
 try {
-$db = new PDO('mysql:host=localhost;dbname=shoppinglist;charset=utf8','root','');
-$db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+$db = openDB();
 $sql = "select * from item";
 $query = $db->query($sql);
 $results = $query->fetchAll(PDO::FETCH_ASSOC);
 header('HTTP/1.1 200 OK');
 print json_encode($results);
 } catch (PDOException $pdoex) {
-    header('HTTP/1.1 500 Internal Server Error');
-    $error = array('error' => $pdoex->getMessage());
-    print json_encode($error);
+    returnError($pdoex);
 }
